841260a199f3a94b5593aaaf2ba8e8df64a72954
Saurav Kumar <srv12797@gmail.com>
2021-11-24 20:24:54 +0530

    Updates

---
+++ precache-manifest.dcb91cc42819d2371e03e6317836c111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88580ad6a83fd209b8b65f25df44762",
+    "url": "/Fusion-An-Online-Streaming-Music-Platform/index.html"
+  },
+  {
+    "revision": "370ac83dc68b766a6d06",
+    "url": "/Fusion-An-Online-Streaming-Music-Platform/static/css/main.7f31ee63.chunk.css"
+  },
+  {
+    "revision": "a64e0663d2e2e52ed8dd",
+    "url": "/Fusion-An-Online-Streaming-Music-Platform/static/js/2.3cdec522.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/Fusion-An-Online-Streaming-Music-Platform/static/js/2.3cdec522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370ac83dc68b766a6d06",
+    "url": "/Fusion-An-Online-Streaming-Music-Platform/static/js/main.6724dd56.chunk.js"
+  },
+  {
+    "revision": "913959df74675640de71",
+    "url": "/Fusion-An-Online-Streaming-Music-Platform/static/js/runtime-main.b2f800a2.js"
+  }
+]);
